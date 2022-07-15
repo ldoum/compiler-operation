75e97c7330cb0b3693125710c9457ba08ca7724a
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2022-07-14 22:14:48 -0400

    Initial commit of lexer mod
    Wanted to build a better lexer. The old version of the lexer in lexer.js is a bit ineffective in scanning the entire input as it requires that each lexeme to be separated by whitespaces for it to tokenize properly.  The old version is also unwieldy and more complex than it had to be since I crammed all of the novel features I thought would be cool to use into that module.

---
+++ lexermod.js
@@ -0,0 +1,143 @@
+//Lexical analysis in a nutshell.
+// 10ish pm 7/13/2022 Wednesday
+//Modified version of lexical analyzer
+
+const { readFileSync } = require('fs');
+
+var everything = readFileSync('./streamer.txt', 'utf-8')
+
+///////////////////ILLUSTRATION ONLY///////////////////////////////////
+
+//character array
+var train = [];
+//input
+for (let index = 0; index < everything.length; index++) {
+	train.push(everything[index]);
+}
+
+console.log('Total number of input characters: ');
+console.log(train.length);
+console.log('Whitespaces to ignore: ');
+console.log(everything.match(/[ ]/g).length);
+
+//////////////////////SCANNING PROCESS////////////////////////////////
+//lexeme array
+var divvy = [];
+const ALPHABET_SOUP = /[A-Za-z]/
+const NUMBER_STEW = /[0-9]/
+const OPERATOR_KEYS = /[-&|!\+\*\/=<>%?]/
+const PUNCTUATION = /[.,;:\[\]\{\}\(\)]/
+const LITERAL_STRING = /["]/
+const NEWLINE = /[\n]/
+const APOSTROPHE = /[']/
+const SPACEBAR = /[ ]/
+
+//scanner
+for (let index = 0; index < train.length; index++) {
+	var char = train[index];
+	var lexeme = ''
+
+	if (char.match(ALPHABET_SOUP)) {
+		
+		while (char.match(ALPHABET_SOUP)) {
+			if (index + 1 == train.length) {
+				break;
+			}
+			
+			lexeme += char;
+			char = train[++index];
+		}
+
+		divvy.push(lexeme);
+		char = train[--index] 
+
+	} else if (char.match(NUMBER_STEW)) {
+		
+		while (char.match(NUMBER_STEW)) {
+			if (index + 1 == train.length) {
+				break;
+			}
+			
+			lexeme += char;
+			char = train[++index];
+		}
+
+		divvy.push(lexeme);
+		char = train[--index] 
+
+	} else if (char.match(OPERATOR_KEYS)) {
+		
+		while (char.match(OPERATOR_KEYS)) {
+			if (index + 1 == train.length) {
+				break;
+			}
+			
+			lexeme += char;
+			char = train[++index];
+		}
+
+		divvy.push(lexeme);
+		char = train[--index] 
+
+	} else if (char.match(PUNCTUATION)) {
+		
+		while (char.match(PUNCTUATION)) {
+			if (index + 1 == train.length) {
+				break;
+			}
+			
+			lexeme += char;
+			char = train[++index];
+		}
+
+		divvy.push(lexeme);
+		char = train[--index] 
+
+	} else if (char.match(LITERAL_STRING)) {
+
+		lexeme += char;
+		char = train[++index];
+
+		while (!char.match(LITERAL_STRING)) {
+			if (index + 1 == train.length) {
+				break;
+			}
+			
+			lexeme += char;
+			char = train[++index];
+		}
+
+		lexeme += char;
+		char = train[++index];
+	
+		divvy.push(lexeme);
+		char = train[--index];
+		
+	} else if (char.match(NEWLINE)) {
+		divvy.push("\n");
+	} else if (char.match(APOSTROPHE)) {
+		divvy.push("'");
+	} else if (char.match(SPACEBAR)) {
+		console.log(`Whitespace found. Index ${index}`)	
+	} else {
+		console.log(`Unknown character present: ${char}`)
+	}
+}
+
+console.log('Lexeme array: ');
+console.log(divvy);
+
+
+//////////////////////TOKENIZING PROCESS////////////////////////////////
+
+//tokens array
+var bucks = [];
+
+//evaluator
+for (let index = 0; index < divvy.length; index++) {
+	var lexeme = divvy[index];
+	bucks.push(lexeme);
+}
+
+console.log('Tokens array: ');
+console.log(bucks);
