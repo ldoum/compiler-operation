307e6d2466eb8a255750d8a6ac3fbfe9f19ed1c9
Lancine Doumbia <107377652+ldoum@users.noreply.github.com>
2022-07-18 22:22:11 -0400

    Second touchup
    The type and values written to tokenizer result file money.txt are stringified.

--- lexermod.js
+++ lexermod.js
@@ -188,7 +188,7 @@ for (let index = 0; index < divvy.length; index++) {
 			case "'":
 				bucks.push({type: 'punctuator' , value: wordup});
 		}
-	} else if (wordup.match(/^[-&|!\+\*\/=<>%?^]+$/)) { //condition updated
+	} else if (wordup.match(/^[-&|!\+\*\/=<>%?^]+$/)) { 
 		switch (wordup){
 			case "||":
 			case "&&":
@@ -246,7 +246,7 @@ for (let index = 0; index < divvy.length; index++) {
 ////////////TOKEN PRINTER//////////////
 var seer = []
 for (let index = 0; index < bucks.length; index++) {
-    seer.push(`{  type: ${Object.values(bucks[index]).join(", value: ").toString()}  }`)
+    seer.push(`{  type: '${Object.values(bucks[index]).join("' , value: '").toString()}'  }`)
 }
 
 try {
